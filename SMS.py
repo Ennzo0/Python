562714bcf996da1412ce50afb4d20f8681f62367
Enzo <127666497+Ennzo0@users.noreply.github.com>
2023-04-23 19:11:52 +0330

    Add files via upload

---
+++ SMS.py
@@ -0,0 +1,8 @@
+#Example: Calculation of SMS cost in Tomans
+#The author of this code: Pouriya Vosough
+
+
+sms=input("inja sms ra paste konid bizahmat :\n")
+result=len(sms)
+result2 = 2.48 *result
+print("hazineh sms shoma",result , "toman mibashad")
